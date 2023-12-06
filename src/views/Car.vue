dd662c1456f4c863e628c927a75037bc09c28574
whale789 <1974396570@qq.com>
2023-12-06 21:57:09 +0800

    first commit

---
+++ src/views/Car.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>这是一个测试{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Car',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
